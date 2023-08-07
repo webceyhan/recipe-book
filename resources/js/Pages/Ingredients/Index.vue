4dd05bce75588868518b91d50627315713327b4a
Evren Ceyhan <evren@ceyhan.io>
2023-08-07 13:46:30 +0300

    Implement recipe / ingredient index ui

---
+++ resources/js/Pages/Ingredients/Index.vue
@@ -0,0 +1,25 @@
+<script setup>
+import GuestLayout from "@/Layouts/GuestLayout.vue";
+import { Head, Link } from "@inertiajs/vue3";
+
+defineProps({
+  ingredients: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
+
+<template>
+  <GuestLayout>
+    <Head title="Ingredients" />
+
+    <h1 class="text-3xl bold">Ingredients</h1>
+
+    <ul>
+      <li v-for="(ingredient, i) in ingredients" :key="i">
+        {{ ingredient.name }}
+      </li>
+    </ul>
+  </GuestLayout>
+</template>
