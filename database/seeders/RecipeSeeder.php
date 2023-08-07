c962959b1b6c702720dbd3759205255a03ff5317
Evren Ceyhan <evren@ceyhan.io>
2023-08-07 13:34:01 +0300

    Add recipe_ingredients seeder

--- database/seeders/RecipeSeeder.php
+++ database/seeders/RecipeSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Enums\Cuisine;
+use App\Models\Ingredient;
+use App\Models\Recipe;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +17,25 @@ class RecipeSeeder extends Seeder
     {
         // populate the database with 1-3 recipes for each cuisine
         foreach (Cuisine::cases() as $cuisine) {
-            \App\Models\Recipe::factory()
+            Recipe::factory()
                 ->count(rand(1, 3))
                 ->create([
                     'cuisine' => $cuisine->value,
                 ]);
+        }
+
+
+        // populate recipe_ingredient table with random ingredients 
+        // and random quantities per each ingredient
+        foreach (Recipe::all() as $recipe) {
+            // take random 3 ingredients
+            $ingredients = Ingredient::all()->random(3);
+
+            foreach ($ingredients as $ingredient) {
+                $recipe->ingredients()->attach($ingredient, [
+                    'quantity' => rand(1, 5),
+                ]);
+            }
         }
     }
 }
